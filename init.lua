e339667014da04672908cafbd338d5bd76bde8ef
jacob.li <li.jacob@inventec.com>
2026-04-14 20:15:48 +0800

    2026-04-14 push

--- init.lua
+++ init.lua
@@ -10,5 +10,36 @@ require("core.options")
 require("core.keymaps")
 require("core.autocmd")
 require("core.terminal")
-require("core.clipboard")
 require("config.lazy")
+
+-- =========================
+-- Font config (single source of truth)
+-- =========================
+local FONT_NAME = "JetBrainsMono Nerd Font"
+local FONT_SIZE = 13
+
+local is_gui = vim.fn.has("gui_running") == 1
+
+if is_gui then
+  -- GUI Neovim only
+  vim.opt.guifont = string.format("%s:h%d", FONT_NAME, FONT_SIZE)
+else
+  -- Terminal Neovim only
+  vim.opt.mouse = "" -- don't steal terminal mouse selection
+end
+
+-- =========================
+-- Common UI (GUI + Terminal)
+-- =========================
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 6
+vim.opt.sidescrolloff = 6
+
+-- Better popup & float experience
+vim.opt.pumblend = 10
+vim.opt.winblend = 10
